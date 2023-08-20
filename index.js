56cd7eda3498394175bde118de489c3c6e53c6e8
Sara <misariguella@gmail.com>
2023-08-20 12:05:52 -0500

    cambio de puertos

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express'); // Importar express.
 const app = express();// Constructor express, se crea la aplicación.
 const path = require('path');
 const ruticas = require('./backend/routes/enrutamiento');
+require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/frontend/views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(7005, () => {
-  console.log('funciona wii');
+app.listen(process.env.PORT || 7988, () => {
+  console.log('funciona ' + process.env.PORT);
 });// .listen: Inicia el servidor, se escucha en el puerto que hay en los parentesis. Se ejecuta la funcion () => cuando el servidor está en línea
 
 app.use('/api/v1/', ruticas);
